701d1bca5d06cc20e64d20753643994ef8dfa78e
DavisKHuehn <davis.huehn@mail.utoronto.ca>
2023-11-29 20:52:42 -1000

    Created LoggedInViewModel and created string name constants for majority of view text

--- interface_adapter/loggedin/LoggedInViewModel.java
+++ interface_adapter/loggedin/LoggedInViewModel.java
@@ -12,6 +12,20 @@ public class LoggedInViewModel extends ViewModel {
      */
     public final String TITLE_BOX = "Logged In View";
 
+    public final String APPLICATION_TITLE = "SkyCast";
+    public final String CITY_SEARCH_DEFAULT_TEXT = "Enter city name here...";
+    public final String CITY_SEARCH_BUTTON_LABEL= "Search City";
+    public final String CREATE_NOTIFICATION_BUTTON_LABEL = "Create Notification";
+    public final String CURRENT_TEMPERATURE_DEFAULT_TEXT = "00";
+    public final String DAILY_MAX_MIN_DEFAULT_TEXT = "(00:00)";
+    public final String DAILY_MAX_MIN_LABEL = "(MAX:MIN) Degrees Celsius";
+    public final String PRECIPITATION_CHANCE_LABEL = "Chance of Percipitation";
+    public final String PRECIPITATION_CHANCE_DEFAULT_TEXT= "00%";
+    public final String CURRENT_TEMPERATURE_LABEL = "Current Temperature (Degrees Celsius)";
+    public final String FUTURE_DATA_DEFAULT_TEXT = "(0: 00:00, 00%)";
+    // TODO: CREATE PROPER LABEL FOR FUTURE_DATA_LABEL
+    public final String FUTURE_DATA_LABEL = "TEMPTITLE (Days Out: MAX:MIN, % Rain)";
+
     private LoggedInState windowState = new LoggedInState();
 
     private final PropertyChangeSupport checkerHelp = new PropertyChangeSupport(this);
