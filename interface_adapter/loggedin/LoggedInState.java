f9fe2a7c4c61cbd01a47389da621221c4fa0021d
DavisKHuehn <davis.huehn@mail.utoronto.ca>
2023-11-29 20:57:16 -1000

    updated LoggedInState to store current city name

--- interface_adapter/loggedin/LoggedInState.java
+++ interface_adapter/loggedin/LoggedInState.java
@@ -2,6 +2,9 @@ package interface_adapter.loggedin;
 
 public class LoggedInState {
     private String username = "";
+
+    private String currentCityName = "";
+
     public LoggedInState(LoggedInState duplicate) {
         username = duplicate.username;
     }
@@ -10,7 +13,10 @@ public class LoggedInState {
     public String getUsername() {
         return username;
     }
+    public String getCurrentCityName() {return currentCityName; }
+
     public void setUsername(String username) {
         this.username = username;
     }
+    public void setCurrentCityName(String cityName) { this.currentCityName = cityName; }
 }
