f0578958ddbbcab3071550c3648331c753beff70
RoyIL <a4834833@gmail.com>
2023-12-01 21:15:28 -0500

    Add location lookup use case interactor

---
+++ use_case/location_lookup/LocationLookupInteractor.java
@@ -0,0 +1,30 @@
+package use_case.location_lookup;
+
+import entity.Location;
+
+public class LocationLookupInteractor implements LocationLookupInputBoundary {
+    final LocationLookupDataAccessInterface accessObject;
+    final LocationLookupOutputBoundary presenter;
+
+    public LocationLookupInteractor(LocationLookupDataAccessInterface accessObject, LocationLookupOutputBoundary presenter) {
+        this.accessObject = accessObject;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void execute(LocationLookupInputData locationLookupInputData) {
+        if(locationLookupInputData.getLocation().isEmpty()) {
+            presenter.prepareFailView("Location cannot be empty!");
+            return;
+        }
+
+        Location location = accessObject.getLocation(locationLookupInputData.getLocation());
+
+        if(location == null) {
+            presenter.prepareFailView("Cannot find the specified location.");
+            return;
+        }
+
+        presenter.prepareSuccessView(new LocationLookupOutputData(location.getName(), location.getRegion(), location.getCountry()));
+    }
+}
