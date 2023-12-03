3eb92caf89522c0e1431771ba6ac6620089f2cf4
RoyIL <a4834833@gmail.com>
2023-12-03 12:46:31 -0500

    Add tests for LocationLookupInteractor

---
+++ test/LocationLookupInteractorTest.java
@@ -0,0 +1,68 @@
+import data_access.WeatherRepository;
+import org.junit.Test;
+import use_case.location_lookup.*;
+
+import static org.junit.Assert.*;
+
+public class LocationLookupInteractorTest {
+    LocationLookupDataAccessInterface repo;
+
+    public LocationLookupInteractorTest() {
+        repo = new WeatherRepository(System.getenv("apiKey"));
+    }
+
+    @Test
+    public void getNormalLocation() {
+        LocationLookupInputData locationData = new LocationLookupInputData("Toronto");
+
+        LocationLookupInputBoundary interactor = getLocationInputBoundary(repo, false, false);
+
+        interactor.execute(locationData);
+    }
+
+    @Test
+    public void getEmptyLocation() {
+        LocationLookupInputData locationData = new LocationLookupInputData("");
+
+        LocationLookupInputBoundary interactor = getLocationInputBoundary(repo, true, true);
+
+        interactor.execute(locationData);
+    }
+
+    @Test
+    public void getNonExistentLocation() {
+        LocationLookupInputData locationData = new LocationLookupInputData("אבחגןחגםשדגגש");
+
+        LocationLookupInputBoundary interactor = getLocationInputBoundary(repo, true, false);
+
+        interactor.execute(locationData);
+    }
+
+    private static LocationLookupInputBoundary getLocationInputBoundary(LocationLookupDataAccessInterface repo, boolean isFailureExpected, boolean isEmpty) {
+        LocationLookupOutputBoundary presenter = new LocationLookupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LocationLookupOutputData data) {
+                if(isFailureExpected) {
+                    fail("Failure was expected");
+                }
+
+                assertEquals(data.getName(), "Toronto");
+                assertEquals(data.getRegion(), "Ontario");
+                assertEquals(data.getCountry(), "Canada");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                if(!isFailureExpected) {
+                    fail("Use case failure is unexpected.");
+                } else if (!isEmpty) {
+                    assertNotEquals(error, "Location cannot be empty!");
+                } else if(isEmpty) {
+                    assertEquals(error, "Location cannot be empty!");
+                }
+            }
+        };
+
+        return new LocationLookupInteractor(repo, presenter);
+    }
+}
