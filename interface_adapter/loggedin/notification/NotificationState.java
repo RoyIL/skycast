510d9d01c13fb076367cab9b2b79fd531492fa3d
DavisKHuehn <davis.huehn@mail.utoronto.ca>
2023-12-02 22:38:13 -1000

    Created View and State for NotificationView

--- interface_adapter/loggedin/notification/NotificationState.java
+++ interface_adapter/loggedin/notification/NotificationState.java
@@ -1,2 +1,32 @@
-package interface_adapter.loggedin.notification;public class NotificationViewState {
+package interface_adapter.loggedin.notification;
+
+public class NotificationState {
+    private String cityName = "";
+    private Boolean wantPrecipitationChance = Boolean.FALSE;
+    private Boolean wantDailyMaxMin = Boolean.FALSE;
+    private Boolean wantCurrentTemperature = Boolean.FALSE;
+
+    private String phoneNumber = "2268689717";
+
+    public NotificationState(NotificationState duplicate) {
+        cityName = duplicate.cityName;
+        wantPrecipitationChance = duplicate.wantPrecipitationChance;
+        wantDailyMaxMin = duplicate.wantDailyMaxMin;
+        wantCurrentTemperature = duplicate.wantCurrentTemperature;
+    }
+
+    public NotificationState() {
+        // void initializer body to differentiate from copy function
+    }
+
+    // Temporary variable in case Daniel's settings are not complete
+
+    public String getCityName() {return cityName;}
+    public Boolean getWantPrecipitationChance() {return wantPrecipitationChance;};
+    public Boolean getWantDailyMaxMin() {return wantDailyMaxMin;}
+    public Boolean getWantCurrentTemperature() {return wantCurrentTemperature;}
+    public void setCityName(String newCityName) {cityName = newCityName;}
+    public void setWantPrecipitationChance(Boolean newBoolValue) {wantPrecipitationChance = newBoolValue;}
+    public void setWantDailyMaxMin(Boolean newBoolValue) {wantDailyMaxMin = newBoolValue;}
+    public void setWantCurrentTemperature(Boolean newBoolValue) {wantCurrentTemperature = newBoolValue;}
 }
--- interface_adapter/loggedin/notification/NotificationViewModel.java
+++ interface_adapter/loggedin/notification/NotificationViewModel.java
@@ -1,2 +1,37 @@
-package interface_adapter.loggedin.notification;public class NotificationViewModel {
+package interface_adapter.loggedin.notification;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class NotificationViewModel extends ViewModel {
+    public final String WINDOW_TITLE = "SkyCast Create Notification";
+    public final String TOGGLE_DAILY_MAX_MIN_LABEL = "Toggle Daily Max and Min Temperatures";
+    public final String TOGGLE_PRECIPITATION_CHANCE_LABEL = "Toggle Precipitation Chance";
+    public final String TOGGLE_CURRENT_TEMPERATURE_LABEL = "Toggle Current Temperature";
+    public final String CITY_NAME_INPUT_LABEL = "City Name";
+    public final String TOGGLE_BUTTON_TRUE_STATE = "ON";
+    public final String TOGGLE_BUTTON_FALSE_STATE = "OFF";
+    public final String CREATE_NOTIFICATION_BUTTON = "Create Notification";
+    public final String CANCEL_NOTIFICATION_BUTTON = "Back to Main Menu";
+
+    private NotificationState windowState = new NotificationState();
+
+    private final PropertyChangeSupport checkerHelp = new PropertyChangeSupport(this);
+
+    public NotificationViewModel() {super("notif");}
+
+    public void setWindowState(NotificationState newWindowState) {this.windowState = newWindowState;}
+
+    public NotificationState getWindowState() {return windowState;}
+
+    public void firePropertyChanged() {
+        checkerHelp.firePropertyChange("windowState", null, this.windowState);
+    }
+
+    public void addPropertyChangedListener(PropertyChangeListener newListener) {
+        checkerHelp.addPropertyChangeListener(newListener);
+    }
+
 }
