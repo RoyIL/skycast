fafa56bfb6dcdf25032ad37a77e67ebc40f07d63
DavisKHuehn <davis.huehn@mail.utoronto.ca>
2023-12-04 06:43:12 -0500

    missed files in my previosu commit

--- src/interface_adapter/loggedin/notification/NotificationViewModel.java
+++ src/interface_adapter/loggedin/notification/NotificationViewModel.java
@@ -4,10 +4,16 @@ import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 public class NotificationViewModel extends ViewModel {
     public final String WINDOW_TITLE = "SkyCast Create Notification";
     public final String TOGGLE_DAILY_MAX_MIN_LABEL = "Toggle Daily Max and Min Temperatures";
+    public final String NOTIFICATION_TIME_LABEL = "Time to Notification (Days, Recommended 0.25-7)";
     public final String TOGGLE_PRECIPITATION_CHANCE_LABEL = "Toggle Precipitation Chance";
     public final String TOGGLE_CURRENT_TEMPERATURE_LABEL = "Toggle Current Temperature";
     public final String CITY_NAME_INPUT_LABEL = "City Name";
@@ -20,7 +26,8 @@ public class NotificationViewModel extends ViewModel {
 
     private final PropertyChangeSupport checkerHelp = new PropertyChangeSupport(this);
 
-    public NotificationViewModel() {super("notif");}
+    public NotificationViewModel() {super("notif");
+    }
 
     public void setWindowState(NotificationState newWindowState) {this.windowState = newWindowState;}
 
